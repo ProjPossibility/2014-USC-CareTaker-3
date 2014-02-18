1f2fd90fe6285cabcf693759edbb11ad39ad058f
Francesca Nannizzi <fnannizzi@gmail.com>
2014-02-17 20:46:58 -0800

    Updated script to format training data from the accelerometer. Currently it leaves the index of the data in, with the plan to strip it out when we parse the data.

--- format_acc_data.py
+++ format_acc_data.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 from __future__ import division
+from collections import deque
 import sys
 import math
 
@@ -15,17 +16,28 @@ def format():
 
     print "Formatting..."
     output_file = open(output_filename, 'w')
+    line_queue = deque()
 
     with open(input_filename) as f:
-        while f.readline():
+        for index in range(0,10):
+            line = f.readline()
+            line = line.rstrip("\n")
+            line = line.replace("/", ",")
+            line_queue.append(line)        
+
+        for line in f:
             output_line = "NOR "
-            for x in range(0,10):
-                line = f.readline()
-                line = line.rstrip("\n")
-                line = line.replace("/", ",")
-                output_line += line
+            for index in range(0,10):      
+                output_line += line_queue[index]
                 output_line += " "
 
             output_file.write("{0}\n".format(output_line))
+            dummy = line_queue.popleft()
+            line = line.rstrip("\n")
+            line = line.replace("/", ",")
+            line_queue.append(line)        
+        
+            
+            
 
 format()
